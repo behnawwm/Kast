be02a146ceffb87051be253769bd4e2272754d44
Behnam Banaei <behban20@gmail.com>
2022-09-20 09:29:09 +0430

    feat(iosApp) : setup sample iosApp

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,28 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+    @ObservedObject private(set) var viewModel: ViewModel
 
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+        Text(viewModel.text)
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var text = "Loading..."
+        init() {
+            Greeting().greeting { greeting, error in
+                DispatchQueue.main.async {
+                    if let greeting = greeting {
+                        self.text = greeting
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
+                    }
+                }
+            }
+        }
+    }
 }
+
+
