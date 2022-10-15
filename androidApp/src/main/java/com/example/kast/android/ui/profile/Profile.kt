11adc4c2074d2d214518e4022faf6e89ce81bd38
Behnam Banaei <behban20@gmail.com>
2022-10-15 17:53:07 +0330

    feat (watchlist) : add placeholder for profile and search screens

--- androidApp/src/main/java/com/example/kast/android/ui/profile/Profile.kt
+++ androidApp/src/main/java/com/example/kast/android/ui/profile/Profile.kt
@@ -1,9 +1,31 @@
-package com.example.kast.android.ui
+package com.example.kast.android.ui.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.kast.android.theme.background
+import com.example.kast.android.theme.orange
 
 @Composable
-fun ProfileScreen() {
-    Text(text = "profile")
+fun ProfileScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(background)
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Profile",color = orange, style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "NOT IMPLEMENTED YET")
+        }
+    }
 }
--- androidApp/src/main/java/com/example/kast/android/ui/search/Search.kt
+++ androidApp/src/main/java/com/example/kast/android/ui/search/Search.kt
@@ -1,8 +1,31 @@
 package com.example.kast.android.ui.search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.kast.android.theme.background
+import com.example.kast.android.theme.orange
 
 @Composable
-fun SearchScreen() {
-
+fun SearchScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(background)
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Search", color = orange, style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "NOT IMPLEMENTED YET")
+        }
+    }
 }
