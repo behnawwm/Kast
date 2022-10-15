a550eacbb6d1f9f14f3c80a7094edaf4cde980d7
Behnam Banaei <behban20@gmail.com>
2022-10-15 10:31:56 +0330

    feat (get-categories) : clean up

--- shared/src/commonMain/kotlin/com/example/kast/domain/model/CategoryType.kt
+++ shared/src/commonMain/kotlin/com/example/kast/domain/model/CategoryType.kt
@@ -5,6 +5,6 @@ sealed class CategoryType(val title: String, val url: String, val detailRoute: S
         CategoryType("Now Playing", "now_playing", "home/more/now_playing")
 
     object Popular : CategoryType("Popular", "popular", "home/more/popular")
-    object TopRated : CategoryType("Top Rated", "top_ratedd", "home/more/top_rated")
+    object TopRated : CategoryType("Top Rated", "top_rated", "home/more/top_rated")
     object Upcoming : CategoryType("Upcoming", "upcoming", "home/more/upcoming")
 }
